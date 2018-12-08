4cf94ab3aa1b73d4e416be9253396d00a3aec4da
dfc <dfrancoce@protonmail.com>
2018-12-08 09:44:30 +0100

    Added swagger to document the API endpoints

--- src/main/java/com/dfc/products/controller/PurchaseOrderController.java
+++ src/main/java/com/dfc/products/controller/PurchaseOrderController.java
@@ -8,6 +8,8 @@ import com.dfc.products.model.PurchaseOrder;
 import com.dfc.products.service.PurchaseOrderProductService;
 import com.dfc.products.service.PurchaseOrderService;
 import com.dfc.products.service.dto.Order;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -24,24 +26,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = "Orders", description = "Set of endpoints for performing operations with orders.")
 public class PurchaseOrderController {
 	@Autowired
 	private PurchaseOrderService purchaseOrderService;
 	@Autowired
 	private PurchaseOrderProductService purchaseOrderProductService;
 
+	@ApiOperation("Creates a new order in the system.")
 	@PostMapping("/orders")
 	@ResponseStatus(HttpStatus.CREATED)
 	public PurchaseOrder create(@RequestBody final PurchaseOrder order) {
 		return purchaseOrderService.create(order);
 	}
 
+	@ApiOperation("Retrieves all the orders stored in the system.")
 	@GetMapping("/orders")
 	@ResponseStatus(HttpStatus.OK)
 	public List<Order> get() {
 		return purchaseOrderService.get();
 	}
 
+	@ApiOperation("Retrieves all the orders, between the dates passed by parameter, stored in the system.")
 	@GetMapping("/orders/period")
 	@ResponseStatus(HttpStatus.OK)
 	public List<Order> get(@RequestParam(value = "fromDate") @DateTimeFormat(pattern = "ddMMyyyy") Date fromDate,
@@ -49,36 +55,42 @@ public class PurchaseOrderController {
 		return purchaseOrderService.get(fromDate, toDate);
 	}
 
+	@ApiOperation("Retrieves the order with the id passed in the path.")
 	@GetMapping("/orders/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Order get(@PathVariable final Long id) {
 		return purchaseOrderService.get(id);
 	}
 
+	@ApiOperation("Updates the order with the id passed in the path.")
 	@PutMapping("/orders/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public PurchaseOrder update(@RequestBody PurchaseOrder order, @PathVariable final Long id) {
 		return purchaseOrderService.update(order, id);
 	}
 
+	@ApiOperation("Deletes from the system the order with the id passed in the path.")
 	@DeleteMapping("/orders/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public void delete(@PathVariable final Long id) {
 		purchaseOrderService.delete(id);
 	}
 
+	@ApiOperation("Adds the products passed in the body to the order with the id passed in the path.")
 	@PutMapping("/orders/{id}/products")
 	@ResponseStatus(HttpStatus.OK)
 	public void addProducts(@RequestBody List<Product> products, @PathVariable final Long id) {
 		purchaseOrderProductService.addProductsToOrder(products, id);
 	}
 
+	@ApiOperation("Deletes the products passed in the body from the order with the id passed in the path.")
 	@DeleteMapping("/orders/{id}/products")
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteProducts(@RequestBody List<Product> products, @PathVariable final Long id) {
 		purchaseOrderProductService.deleteProductsFromOrder(products, id);
 	}
 
+	@ApiOperation("Calculates the total for the products of the order with the id passed in the path.")
 	@GetMapping("/orders/{id}/calculate")
 	@ResponseStatus(HttpStatus.OK)
 	public String calculate(@PathVariable final Long id) {
